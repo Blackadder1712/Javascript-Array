4afb40af4e849dc2f97b6b19624cca262cf87982
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-11-21 13:27:04 +0000

    object
    still trying to target object withing array

--- Javascript/xhr.js
+++ Javascript/xhr.js
@@ -1,72 +1,57 @@
-const getImg = document.getElementById('get-img');
-const postImg = document.getElementById('post-img');
-
-const sendHttpRequest = (method, url, data) =>
+function SearchPhotos()
 {
-    const promise = new Promise((resolve, reject) => 
-    {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-    
-        xhr.responseType = 'json';
-        
-        if (data)
-            {
-                xhr.setRequestHeader('Content-Type', 'application/json'); // appends data
-            }
-     
-        
-        xhr.onload = function()
-        {
-            if(xhr.status >= 400)//check response status
-              {
-                reject(xhr.response);
-              }
-
-            else //if no errors
-              {
-                resolve(xhr.response);
-              }
-          
-        };
-
-        xhr.onerror = () =>
-        {
-           reject('Something went wrong!'); // if there is an error
-        };
-
-
-    
-        xhr.send(JSON.stringify(data));
-    });
-
-    return promise;
-
    
-};
 
-const getData = () =>
-{
-    sendHttpRequest('GET', 'https://picsum.photos').then(responseData =>
+
+    
+    let url = "https://picsum.photos/v2/list?page=2&limit=100";
+    let image = document.querySelector(".image");
+    let random = Math.floor(Math.random() *(10) +1)
+
+  
+    
+
+   //make request to API
+
+    fetch(url)
+    .then(function (data)
     {
+      return data.json();
 
-    });
-};
+      
+     
 
-const sendData = () =>
-{
-   sendHttpRequest('POST', 'https://picsum.photos',
-   {
-      email: 'test@test.com'
-   }).then(responseData =>
+      
+      
+       
+       
+    }
+    )
+
+     .then(function(data)
     {
+   
+        console.log(data);
 
-    }).catch(err =>
-        {
-           console.log(err); 
-        });//deal with error
+        const html =`<img class="image" src="${data.download_url}">` 
 
-};
+        image.insertAdjacentHTML("beforeend", html)
+   
 
-getImg.addEventListener('click', getData);
-postImg.addEventListener('click', sendData);
+
+
+        
+        
+        
+    })
+    
+ 
+
+  
+
+
+
+
+
+ 
+}
