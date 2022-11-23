97b0697bbb1ab0a8a8a16548cb7388ba3a8b85fe
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-11-23 12:09:34 +0000

    email arrays
    emails are now placed within array when entered (if accepted by regex)

---
+++ Javascript/email.js
@@ -0,0 +1,66 @@
+
+//regex variables 
+let emailInput = document.getElementById('email');
+let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
+   $("#send").click(function()
+   {
+            //check if email field contains @ character
+            if 
+            (emailInput.value.match(emailRegex) )
+            {
+                $("#email").css({"border": "3px solid #7FFF94"})
+                {
+                    var x = document.getElementById("email").value;
+                    let emails = [];
+                    emails.unshift(x);
+
+                    console.log(emails)
+
+                 
+                    
+       
+                    
+                
+
+                  
+                    
+                    console.log(emails);
+
+                    
+
+              
+                    document.getElementById("text").innerHTML = x;
+
+                    return;
+
+                 
+
+                }
+
+       
+               
+                
+              
+
+            }
+
+            else
+           {
+              $("#email").css({"border": "3px solid red"})
+              {
+                alert("Please enter valid email address")
+              }
+
+            }
+
+          
+
+
+            });
+          
+
+  
+
+              
